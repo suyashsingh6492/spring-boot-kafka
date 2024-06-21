45c6b341d4c55e595e9a6f843129fe9fb2aec49c
suyash kumar singh <suyash.singh-ext@guardhat.com>
2024-06-21 18:47:41 +0530

    updated consumer

---
+++ device-event-consumer/src/main/java/com/kafka/device/DeviceEventConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.device;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceEventConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceEventConsumerApplication.class, args);
+	}
+
+}
--- device-event-producer/src/main/java/com/kafka/device/controller/DeviceEventsController.java
+++ device-event-producer/src/main/java/com/kafka/device/controller/DeviceEventsController.java
@@ -2,11 +2,13 @@ package com.kafka.device.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kafka.device.domain.DeviceEvent;
+import com.kafka.device.domain.DeviceEventType;
 import com.kafka.device.producer.DeviceEventProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +36,29 @@ public class DeviceEventsController {
         deviceEventProducer.sendDeviceEvent_sendObject(deviceEvent);
         log.info("After sending deviceEvent.");
         return ResponseEntity.status(HttpStatus.CREATED).body(deviceEvent);
+    }
+
+    @PutMapping("/v1/device/event")
+    public ResponseEntity<?> updateDeviceEvent(
+            @RequestBody DeviceEvent deviceEvent
+    ) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+        log.info("deviceEvent {}", deviceEvent);
+        ResponseEntity<String> BAD_REQUEST = validateUpdateDeviceEvent(deviceEvent);
+        if (BAD_REQUEST != null) return BAD_REQUEST;
+        deviceEventProducer.sendDeviceEvent_sendObject(deviceEvent);
+        log.info("After sending deviceEvent.");
+        return ResponseEntity.status(HttpStatus.OK).body(deviceEvent);
+    }
+
+    private static ResponseEntity<String> validateUpdateDeviceEvent(DeviceEvent deviceEvent) {
+        if(deviceEvent.deviceEventId()==null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass the library event id");
+        }
+        if(!DeviceEventType.UPDATE.equals( deviceEvent.deviceEventType())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only UPDATE event type is supported");
+
+        }
+        return null;
     }
 
 }
