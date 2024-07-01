f2d14f495aa05c18cadede95853e854836306b0d
suyash kumar singh <suyash.singh-ext@guardhat.com>
2024-07-01 19:56:12 +0530

    updated test cases

---
+++ device-event-producer/src/test/java/unit/com/kafka/device/controller/DeviceEventsControllerTest.java
@@ -0,0 +1,13 @@
+package com.kafka.device.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeviceEventsControllerTest {
+
+    @Test
+    void postDeviceEvent() {
+        
+    }
+}
